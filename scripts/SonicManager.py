616dc66aa19e3b74a9a91d2d0890dfb5a236eeed
JioBani <spdlqj8876@gmail.com>
2022-11-23 00:46:09 +0900

    no message

--- scripts/SonicManager.py
+++ scripts/SonicManager.py
@@ -2,11 +2,13 @@ import time
 import RPi.GPIO as GPIO
 import SonicClass
 
+n = 0
+
 def onIn() :
   print("들어옴")
 
 def onOut(time) :
-  print("나감 : %f" % time)
+  print("%d) 나감 : %f" % n,time)
 
 
 # 전역 변수 선언 및 초기화
