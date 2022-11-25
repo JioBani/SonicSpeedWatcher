6a59bc131bb3da45a85ecb3f7efe7a01f6632790
JioBani <spdlqj8876@gmail.com>
2022-11-25 19:57:19 +0900

    no message

--- scripts/SensorTester.py
+++ scripts/SensorTester.py
@@ -9,6 +9,7 @@ gpioManager.init()
 
 class SonicThread(Thread):
   def __init__(self , trigger, echo):
+    self.daemon = True
     Thread.__init__(self)
     self.trigger = trigger
     self.echo = echo
