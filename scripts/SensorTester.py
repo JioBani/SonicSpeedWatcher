cd5de85c3ca27f7abbb97213cd1b852829793d79
JioBani <spdlqj8876@gmail.com>
2022-11-25 16:24:18 +0900

    센서 테스트 수정

--- scripts/SensorTester.py
+++ scripts/SensorTester.py
@@ -8,5 +8,5 @@ gpioManager.init()
 def onOut(time):
   print("감지")
 
-enterSonic = SonicClass.Sonic(GpioManager.enterTrigger,GpioManager.enterEcho, 1000 ,onOut)
+enterSonic = SonicClass.Sonic(GpioManager.exitTrigger,GpioManager.exitEcho, 1000 ,onOut)
 enterSonic.startRun()
