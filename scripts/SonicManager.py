14db49415d3f31cd0070e3789dc4cccd08b3875c
JioBani <spdlqj8876@gmail.com>
2022-11-25 23:00:01 +0900

    no message

--- scripts/SonicManager.py
+++ scripts/SonicManager.py
@@ -9,16 +9,18 @@ def onOut(time) :
   print("나감 : %f" % time)
 
 def onPassEnter(time):
-  global passTime
+  global passTime, exitSonic, enterSonic
   enterSonic.stop()
   print("입장 시간 : %f" % time)
   passTime = time
   exitSonic.start()
 
 def onPassExit(time):
+  global passTime, exitSonic, enterSonic
   exitSonic.stop()
   print("퇴장 시간 : %f" % time)
   print("걸린 시간 : %f" % (time - passTime))
+  enterSonic = Sonic(GpioManager.enterTrigger,GpioManager.enterEcho, 1000 ,onPassEnter)
   enterSonic.start()
 
 # 전역 변수 선언 및 초기화
