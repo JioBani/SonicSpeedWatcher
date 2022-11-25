e90748ef5787daff36a3f034af9a32898fd0e2fe
JioBani <spdlqj8876@gmail.com>
2022-11-25 16:29:14 +0900

    no message

--- scripts/SensorTester.py
+++ scripts/SensorTester.py
@@ -1,6 +1,7 @@
 import time
 import SonicClass
 from GpioManager import GpioManager
+import RPi.GPIO as GPIO
 
 gpioManager = GpioManager()
 gpioManager.init()
@@ -8,7 +9,26 @@ gpioManager.init()
 def onOut(time):
   print("감지")
 
-enterSonic = SonicClass.Sonic(GpioManager.exitTrigger,GpioManager.exitEcho, 1000 ,onOut)
-enterSonic.startRun()
+def measureDistance(self):
+
+    '''
+    물체의 거리를 mm 단위로 반환
+    '''
+
+    GPIO.output(GpioManager.exitTrigger, True) # 신호 1 발생
+    GPIO.output(GpioManager.exitTrigger, False) # 신호 0 발생(falling 에지)
+
+    while(GPIO.input(GpioManager.exitEcho) == 0):
+        pass
+    pulse_start = time.time() # 신호 1. 초음파 발생이 시작되었음을 알림
+    while(GPIO.input(GpioManager.exitEcho) == 1):
+        pass
+    pulse_end = time.time() # 신호 0. 초음파 수신 완료를 알림
+
+    pulse_duration = pulse_end - pulse_start
+    return 340*10000/2*pulse_duration
+
+#enterSonic = SonicClass.Sonic(GpioManager.exitTrigger,GpioManager.exitEcho, 1000 ,onOut)
+#enterSonic.startRun()
 while True:
-  pass
+  print(measureDistance)
