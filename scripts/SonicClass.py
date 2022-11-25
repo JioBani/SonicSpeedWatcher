888d839a2c6ef16c4b3b8a5e241b2ddb52c1f1e8
JioBani <spdlqj8876@gmail.com>
2022-11-25 16:41:12 +0900

    no message

--- scripts/SonicClass.py
+++ scripts/SonicClass.py
@@ -39,6 +39,7 @@ class Sonic:
         inTime = 0
         while self.run:
             distance = self.measureDistance()
+            print(distance)
             if(isIn) :
                 if(distance > self.triggerDistance) :
                     outTime =  time.time()
