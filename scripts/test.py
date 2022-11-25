2be4920e7b31bddd368219098530a5cecbc50283
JioBani <spdlqj8876@gmail.com>
2022-11-25 20:49:13 +0900

    no message

--- scripts/test.py
+++ scripts/test.py
@@ -1,13 +1,26 @@
-class Person:
+import time
+from threading import Thread
+class Sonic(Thread):
 
-  def __init__(self, _name):
-    self.name = _name
+    def __init__(self):
+        Thread.__init__(self)
 
-  def PrintName(self):
-    print(self.name)
+    def measureDistance(self):
+      return 1
 
-  def Print(self):
-    self.PrintName()
+    def run(self):
+        print("시작")
+        i = 0;
+        while True:
+            i = i + self.measureDistance()
+            #print(distance)
+            if(i > 1000) : print("넘음")
 
-person = Person("gd")
-person.Print()
+sonic = Sonic()
+sonic.start()
+
+#sonic = NThreadSonic(GpioManager.enterTrigger , GpioManager.enterEcho , onOut)
+#sonic.run()
+
+while True:
+    pass
