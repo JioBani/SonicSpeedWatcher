851879b51780c1667d58d7f650db2a43c5a00185
JioBani <spdlqj8876@gmail.com>
2022-11-25 22:40:39 +0900

    no message

--- scripts/SonicManager.py
+++ scripts/SonicManager.py
@@ -2,17 +2,21 @@ import time
 from Sonic import Sonic
 from GpioManager import GpioManager
 
+passTime = 0
+
 def onOut(time) :
   print("나감 : %f" % time)
 
 def onPassEnter(time):
   #exitSonic.startRun()
   print("입장 시간 : %f" % time)
+  passTime = time
   pass
 
 def onPassExit(time):
   #exitSonic.stopRun()
   print("퇴장 시간 : %f" % time)
+  print("걸린 시간 : %f" % time - passTime)
   pass
 
 # 전역 변수 선언 및 초기화
