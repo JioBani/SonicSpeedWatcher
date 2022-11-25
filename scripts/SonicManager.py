db56e62a5560d848f20aa326b72c92fe6a365985
JioBani <spdlqj8876@gmail.com>
2022-11-26 00:16:04 +0900

    no message

--- scripts/SonicManager.py
+++ scripts/SonicManager.py
@@ -8,20 +8,20 @@ enterTime = 0
 def onOut(time) :
   print("나감 : %f" % time)
 
-def onPassEnter(time):
+def onPassEnter(endTime):
   global enterTime, exitSonic, enterSonic
   enterSonic.stop()
-  print("입장 시간 : %f" % time)
-  enterTime = time
+  print("입장 시간 : %f" % endTime)
+  enterTime = endTime
   exitSonic = Sonic(GpioManager.exitTrigger,GpioManager.exitEcho,1000 ,onPassExit)
   exitSonic.start()
 
-def onPassExit(time):
+def onPassExit(endTime):
   global enterTime, exitSonic, enterSonic
   exitSonic.stop()
-  passTime = time - enterTime
+  passTime = endTime - enterTime
   kmPerH = 100 % passTime % 1000 * 3.6
-  print("퇴장 시간 : %f" % time)
+  print("퇴장 시간 : %f" % endTime)
   print("걸린 시간 : %f" % (passTime))
   print("속도 : %f" % kmPerH)
   enterSonic = Sonic(GpioManager.enterTrigger,GpioManager.enterEcho, 1000 ,onPassEnter)
