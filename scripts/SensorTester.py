952f2504c4191ffd425582d233225a0ec9773f3c
JioBani <spdlqj8876@gmail.com>
2022-11-25 16:37:22 +0900

    no message

--- scripts/SensorTester.py
+++ scripts/SensorTester.py
@@ -26,8 +26,26 @@ def measureDistance():
     return 340*10000/2*pulse_duration
 '''
 
+def measureDistance(trigger , echo):
 
-enterSonic = SonicClass.Sonic(GpioManager.exitTrigger,GpioManager.exitEcho, 1000 ,onOut)
-enterSonic.startRun()
+    GPIO.output(trigger, True) # 신호 1 발생
+    GPIO.output(trigger, False) # 신호 0 발생(falling 에지)
+
+    while(GPIO.input(echo) == 0):
+        pass
+    pulse_start = time.time() # 신호 1. 초음파 발생이 시작되었음을 알림
+    while(GPIO.input(echo) == 1):
+        pass
+    pulse_end = time.time() # 신호 0. 초음파 수신 완료를 알림
+
+    pulse_duration = pulse_end - pulse_start
+    return 340*10000/2*pulse_duration
+
+
+#enterSonic = SonicClass.Sonic(GpioManager.enterTrigger,GpioManager.enterEcho, 1000 ,onOut)
+#exitSonic = SonicClass.Sonic(GpioManager.exitTrigger,GpioManager.exitEcho,1000 ,onOut)
+
 while True:
-  input()
+  measureDistance(GpioManager.enterTrigger , GpioManager.enterEcho)
+  measureDistance(GpioManager.exitTrigger ,GpioManager.exitEcho )
+  time.sleep(1)
