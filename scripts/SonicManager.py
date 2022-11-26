d9bd8f605352905d441f86179cc872a679ef0fd6
JioBani <spdlqj8876@gmail.com>
2022-11-26 17:02:12 +0900

    onPass 적용

--- scripts/SonicManager.py
+++ scripts/SonicManager.py
@@ -9,6 +9,7 @@ class SonicManager:
         self.enterSonic = Sonic(GpioManager.enterTrigger,GpioManager.enterEcho, 1000 ,self.onPassEnter)
         self.exitSonic = Sonic(GpioManager.exitTrigger,GpioManager.exitEcho,1000 ,self.onPassExit)
         self.enterTime = 0
+        self.onPass
 
     def onPassEnter(self,endTime):
         self.enterSonic.stop()
@@ -24,6 +25,7 @@ class SonicManager:
         print("퇴장 시간 : %f" % endTime)
         print("걸린 시간 : %f" % (passTime))
         print("속도 : %f" % kmPerH)
+        self.onPass(endTime , passTime , kmPerH)
         self.enterSonic = Sonic(GpioManager.enterTrigger,GpioManager.enterEcho, 1000 ,self.onPassEnter)
         self.enterSonic.start()
 
@@ -32,4 +34,5 @@ class SonicManager:
         while True :
             input("멈추려면 아무키나 눌러주세요")
 
-GpioManager.init()
+    def setOnPass(self , onPass):
+        self.onPass = onPass
--- scripts/SystemManager.py
+++ scripts/SystemManager.py
@@ -1,6 +1,12 @@
 from SonicManager import *
 
+def onPass(exitTime, passTime, velocity):
+  print("퇴장 시각 : %f" , exitTime)
+  print("통과 시간 : %f" , passTime)
+  print("평균 속도 : %f" , velocity)
+
 GpioManager.init()
 
 sonicManager = SonicManager()
+sonicManager.setOnPass(onPass=onPass)
 sonicManager.run()
