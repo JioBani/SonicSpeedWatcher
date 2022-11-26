80e5cd816bc48ad5caf0ef9fbfdf0e23c8a00089
JioBani <spdlqj8876@gmail.com>
2022-11-26 17:36:50 +0900

    no message

--- scripts/DataIO.py
+++ scripts/DataIO.py
@@ -13,9 +13,14 @@ class DataManager():
         self.saveData(path=passDataPath,data=passData)
 
     def readPassData(self):
-        with open("../static/data/passData.bin", 'rb') as file:    # james.p 파일을 바이너리 읽기 모드(rb)로 열기
-            passData = pickle.load(file)
-            return passData
+        with open("../static/data/passData.bin", 'rb') as file:
+            data = []
+            while True:  # james.p 파일을 바이너리 읽기 모드(rb)로 열기
+                try :
+                    passData = pickle.load(file)
+                    data.append(passData)
+                except EOFError :
+                    return data
 
 dataManager = DataManager()
 passData = dataManager.readPassData()
