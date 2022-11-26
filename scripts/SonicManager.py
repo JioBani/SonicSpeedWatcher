1139a8a48bb68f4b5ca891ab3741cd9a15fbba54
JioBani <spdlqj8876@gmail.com>
2022-11-26 17:02:39 +0900

    no message

--- scripts/SonicManager.py
+++ scripts/SonicManager.py
@@ -9,7 +9,7 @@ class SonicManager:
         self.enterSonic = Sonic(GpioManager.enterTrigger,GpioManager.enterEcho, 1000 ,self.onPassEnter)
         self.exitSonic = Sonic(GpioManager.exitTrigger,GpioManager.exitEcho,1000 ,self.onPassExit)
         self.enterTime = 0
-        self.onPass
+        self.onPass = None
 
     def onPassEnter(self,endTime):
         self.enterSonic.stop()
@@ -22,9 +22,6 @@ class SonicManager:
         self.exitSonic.stop()
         passTime = endTime - self.enterTime
         kmPerH = 200 / passTime / 1000 * 3.6
-        print("퇴장 시간 : %f" % endTime)
-        print("걸린 시간 : %f" % (passTime))
-        print("속도 : %f" % kmPerH)
         self.onPass(endTime , passTime , kmPerH)
         self.enterSonic = Sonic(GpioManager.enterTrigger,GpioManager.enterEcho, 1000 ,self.onPassEnter)
         self.enterSonic.start()
