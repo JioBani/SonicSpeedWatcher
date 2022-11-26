df4ae22b8a34a53186edf062b53225a5660abd23
JioBani <spdlqj8876@gmail.com>
2022-11-26 22:57:35 +0900

    no message

--- scripts/SonicManager.py
+++ scripts/SonicManager.py
@@ -2,7 +2,7 @@ import time
 from Sonic import Sonic
 from GpioManager import GpioManager
 import signal
-
+import multiprocessing as mp
 
 class SonicManager:
     def __init__(self):
@@ -12,24 +12,41 @@ class SonicManager:
         self.enterTime = 0
         self.onPass = None
 
-    def onPassEnter(self,endTime):
-        if(self.enterSonic.process != None) :
-            self.enterSonic.stop()
+        self.enterProcess = mp.Process(name="EnterProcess",target=self.enterSonic.sonicSubLoop)
+        self.exitProcess = None
+
+    def onPassEnter(self,endTime) :
+        self.enterProcess.terminate()
         print("입장 시간 : %f" % endTime)
         self.enterTime = endTime
-        self.exitSonic = Sonic(GpioManager.exitTrigger,GpioManager.exitEcho,1000 ,self.onPassExit)
-        self.exitSonic.start()
+        self.exitProcess = mp.Process(name="ExitProcess",target=self.exitProcess.sonicSubLoop)
+        self.exitProcess.start()
+
+#        self.enterSonic.stop()
+#        print("입장 시간 : %f" % endTime)
+#        self.enterTime = endTime
+#       self.exitSonic = Sonic(GpioManager.exitTrigger,GpioManager.exitEcho,1000 ,self.onPassExit)
+#        self.exitSonic.start()
 
     def onPassExit(self,endTime):
-        self.exitSonic.stop()
+        self.exitProcess.terminate()
         passTime = endTime - self.enterTime
         kmPerH = 200 / passTime / 1000 * 3.6
         self.onPass(endTime , passTime , kmPerH)
-        self.enterSonic = Sonic(GpioManager.enterTrigger,GpioManager.enterEcho, 1000 ,self.onPassEnter)
-        self.enterSonic.start()
+        self.enterProcess = mp.Process(name="EnterProcess",target=self.enterSonic.sonicSubLoop)
+        self.enterProcess.start()
+
+#        self.exitSonic.stop()
+#        passTime = endTime - self.enterTime
+#        kmPerH = 200 / passTime / 1000 * 3.6
+#        self.onPass(endTime , passTime , kmPerH)
+#        self.enterSonic = Sonic(GpioManager.enterTrigger,GpioManager.enterEcho, 1000 ,self.onPassEnter)
+#        self.enterSonic.start()
+
+
 
     def run(self):
-        self.enterSonic.start()
+        self.enterProcess.start()
         while True :
             input("멈추려면 아무키나 눌러주세요")
 
