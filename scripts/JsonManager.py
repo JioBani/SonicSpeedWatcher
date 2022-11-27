f15b5a7fc2fd20507f40c5efaaa47b1b2600596c
JioBani <spdlqj8876@gmail.com>
2022-11-27 17:43:38 +0900

    Json 완성

--- scripts/JsonManager.py
+++ scripts/JsonManager.py
@@ -2,11 +2,11 @@ import json
 import schema.PassData
 
 def dictionaryToJson(dict):
-  return json.dumps(dict)
+    return json.dumps(dict)
 
 def passDataArrayToJson(arr):
-  resultDict = {}
-  for passData in arr:
-    dict = passData.getDict()
-    resultDict[dict['exitTime']] = dict
-  return json.dumps(resultDict)
+    resultDict = {}
+    for passData in arr:
+        dict = passData.getDict()
+        resultDict[dict['exitTime']] = dict
+    return json.dumps(resultDict)
