30b7cb5c186c50e86b842c45dc2af365f2469c67
JioBani <spdlqj8876@gmail.com>
2022-11-27 19:30:10 +0900

    no message

--- scripts/MqttClient.py
+++ scripts/MqttClient.py
@@ -1,14 +1,11 @@
 import paho.mqtt.client as mqtt
 
-def onConnect(client, userData, flag, rc):
-    print("Connect with result code:"+ str(rc))
-
 class MqttClient:
 
     def __init__(self, ip, onMessage = None):
         self.ip = ip
         self.client = mqtt.Client()
-        #self.client.on_connect = onConnect
+        self.client.on_connect = self.onConnect
         self.client.on_message = onMessage
 
     def onConnect(self,client, userData, flag, rc):
@@ -21,4 +18,4 @@ class MqttClient:
         self.client.publish(topic=topic,payload=msg,qos=0)
 
     def run(self):
-        self.onConnect(self.ip , 1883)
+        self.client.connect(self.ip , 1883)
