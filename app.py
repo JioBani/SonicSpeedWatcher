bd6ed7c38662c7e643762adbd7d5575fe45006be
JioBani <spdlqj8876@gmail.com>
2022-11-28 21:16:44 +0900

    no message

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-        return render_template('13-3chart.html')
+        return render_template('view_data.html')
+
+@app.route('/view_data')
+def index():
+        return render_template('view_data.html')
+
+@app.route('/setting')
+def index():
+        return render_template('setting.html')
+
+@app.route('/view_with_image')
+def index():
+        return render_template('view_with_image.html')
+
 
 if __name__ == "__main__":
         app.run(host='0.0.0.0', port=8080)
